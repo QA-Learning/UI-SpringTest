c28c802c824f20ff4b474b6aa86d140acd469391
Swati Kalra <swati.kalra@Swatis-MacBook-Pro.local>
2020-05-30 16:00:33 +0530

    adding JenkinsFile

--- src/test/java/juiceshop_e2e.java
+++ src/test/java/juiceshop_e2e.java
@@ -1,7 +1,9 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -11,37 +13,43 @@ public class juiceshop_e2e {
     String email = "b@gmail.com";
     String password = "abc@gmail.com";
 
-    @Test
-    public void register(){
-        System.setProperty("webdriver.chrome.driver","/Users/swati.kalra/Downloads/chromedriver");
+    @BeforeTest
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://localhost:3000/#/register");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//*[@id=\"mat-dialog-0\"]/app-welcome-banner/div/div[2]/button[2]")).click();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        driver.findElement(By.id("emailControl")).sendKeys(email);
-        driver.findElement(By.id("passwordControl")).sendKeys(password);
-        driver.findElement(By.id("repeatPasswordControl")).sendKeys(password);
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        driver.findElement(By.name("securityQuestion")).click();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//span[@class='mat-option-text']")).click();
-        driver.findElement(By.id("securityAnswerControl")).sendKeys(password);
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        driver.findElement(By.id("registerButton")).click();
-
-
     }
+
+//    @Test
+//    public void register(){
+//        driver.get("http://localhost:3000/#/register");
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        driver.findElement(By.xpath("//*[@id=\"mat-dialog-0\"]/app-welcome-banner/div/div[2]/button[2]")).click();
+//        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+//        driver.findElement(By.id("emailControl")).sendKeys(email);
+//        driver.findElement(By.id("passwordControl")).sendKeys(password);
+//        driver.findElement(By.id("repeatPasswordControl")).sendKeys(password);
+//        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+//        driver.findElement(By.name("securityQuestion")).click();
+//        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+//        driver.findElement(By.xpath("//span[@class='mat-option-text']")).click();
+//        driver.findElement(By.id("securityAnswerControl")).sendKeys(password);
+//        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+//        driver.findElement(By.id("registerButton")).click();
+//    }
 
     @Test
     public void login(){
-    
-    
-    
-    
+        driver.get("http://localhost:3000/#/login");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.id("email")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//*[@id='mat-dialog-0']/app-welcome-banner/div/div[2]/button[2]")).click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.findElement(By.id("loginButton")).click();
     }
 
+    @Test
     public void addItemToCart(){
 
     }
